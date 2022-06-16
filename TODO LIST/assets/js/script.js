615efc8902519f7ba11c6e153b64f2aa8a5f9467
Banditsi <103197256+Banditsi@users.noreply.github.com>
2022-06-17 00:35:23 +0300

    Add files via upload

--- TODO LIST/assets/js/script.js
+++ TODO LIST/assets/js/script.js
@@ -26,9 +26,37 @@ document.querySelector('#push').onclick = function(){
             tasks[i].onclick = function(){
                 this.classList.toggle('completed');
             }
-        }
-
+        }    
 
         document.querySelector("#newtask input").value = "";
     }
 }
+
+const filterOption = document.querySelector(".filter-todo");
+
+filterOption.addEventListener('click', filterTodo)
+
+function filterTodo(e){
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo){
+        switch(e.target.value){
+            case "all":
+                todo.style.display = "flex"
+                break;
+                case "completed":
+                    if(todo.classList.contains('completed')){
+                        todo.style.display = "flex";
+                    }else{
+                        todo.style.display = "none";
+                    }
+                    break;
+                    case "uncompleted":
+                        if(!todo.classList.contains('completed')){
+                            todo.style.display = "flex";
+                        } else{
+                            todo.style.display = "none";
+                        }
+                        break;
+        }
+    });
+} 
